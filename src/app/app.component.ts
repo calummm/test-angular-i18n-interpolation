f6e832772671b88d971de883ff07e7743da41131
Calum Moir-Mattox <calummm@gmail.com>
2021-04-28 16:17:38 +1000

    Rebuild with ng new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,10 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
-  readonly firstTest = '1';
-  readonly secondTest = '2';
-  readonly thirdTest = 3;
-  readonly forthTest = 4;
+export class AppComponent {
+  title = 'test-angular-i18n-interpolation';
 }
